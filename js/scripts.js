d2d4d92db5d15db8e695ce48afb3f1ab5703f561
Chris Gosser and William Wittenbrock <chris@>
2018-08-07 16:13:27 -0700

    Add picture of dog and cat. When they are clicked, they say meow and bark.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $("#bark").click(function() {
+    $("#doggo").prepend("<li>Bark! Bark! Bark!</li>");
+    $("#kitty").prepend("<li>Meow! Meow! Meow!!</li>");
+  });
+
+  $("#meow").click(function() {
+    $("#kitty").prepend("<li>Meow! Meow! Meow!!</li>");
+    $("#doggo").prepend("<li>Bark! Bark! Bark!</li>");
+  });
+
+
+
+
+  $("button#green").click(function() {
+   $("body").removeClass();
+   $("body").addClass("green-background");
+ });
+
+ $("button#normal").click(function() {
+  $("body").removeClass();
+});
+
+});

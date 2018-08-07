ddaa95403e392abffe3a245d7c781c00f65204b2
Chris Gosser and William Wittenbrock <chris@>
2018-08-07 16:47:28 -0700

    Peer review edits fixed some js script errors.

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,12 @@ $(document).ready(function() {
 
 
   $("button#green").click(function() {
-   $("body").removeClass();
-   $("body").addClass("green-background");
- });
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
 
- $("button#normal").click(function() {
-  $("body").removeClass();
-});
+  $("button#normal").click(function() {
+    $("body").removeClass();
+  });
 
 });
